0bcf4171f616ff0e96b90f3513f9aada2c177ae4
I-Maged <magedhanafy@gmail.com>
2022-11-02 14:47:05 +0200

    added forecast

--- src/APICONTROL.js
+++ src/APICONTROL.js
@@ -1,23 +1,30 @@
 import { displayWeather } from './UICONTROL';
 
-async function getWeatherByPosition(lat, lon) {
-  const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OWM_API}&units=metric`
-  );
-  if (res.status === 200) {
-    let data = await res.json();
-    displayWeather(data);
-  }
-}
+const fetchWeatherData = async (searcData, searchFrom) => {
+  const URL = 'https://api.openweathermap.org/data/2.5';
+  let fetchCurrent;
+  let fetchForecast;
 
-async function getWeatherByCityName(cityName) {
-  const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.OWM_API}&units=metric`
-  );
-  if (res.status === 200) {
-    let data = await res.json();
-    displayWeather(data);
+  if (searchFrom == 'cityName') {
+    fetchCurrent = await fetch(
+      `${URL}/weather?q=${searcData}&appid=${process.env.OWM_API}&units=metric`
+    );
+    fetchForecast = await fetch(
+      `${URL}/forecast?q=${searcData}&appid=${process.env.OWM_API}&units=metric`
+    );
+  } else if (searchFrom == 'coords') {
+    fetchCurrent = await fetch(
+      `${URL}/weather?lat=${searcData[0]}&lon=${searcData[1]}&appid=${process.env.OWM_API}&units=metric`
+    );
+    fetchForecast = await fetch(
+      `${URL}/forecast?lat=${searcData[0]}&lon=${searcData[1]}&appid=${process.env.OWM_API}&units=metric`
+    );
   }
-}
 
-export { getWeatherByPosition, getWeatherByCityName };
+  const current = await fetchCurrent.json();
+  const forecast = await fetchForecast.json();
+
+  displayWeather(current, forecast);
+};
+
+export { fetchWeatherData };
--- src/UICONTROL.js
+++ src/UICONTROL.js
@@ -1,4 +1,4 @@
-import { getWeatherByPosition, getWeatherByCityName } from './APICONTROL';
+import { fetchWeatherData } from './APICONTROL';
 
 const setDom = {
   nameField: document.querySelector('.city-name-input'),
@@ -12,7 +12,8 @@ function getCoordinates() {
   function success(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    getWeatherByPosition(lat, lon);
+
+    fetchWeatherData([lat, lon], 'coords');
   }
 
   function error() {
@@ -31,22 +32,65 @@ function getCityName() {
     setDom.nameField.value = '';
     console.log('empty field');
   } else {
-    getWeatherByCityName(setDom.nameField.value);
+    fetchWeatherData(setDom.nameField.value, 'cityName');
   }
 }
 
-function displayWeather(data) {
-  const { name } = data;
-  const { country } = data.sys;
-  const { icon, main } = data.weather[0];
-  const { temp } = data.main;
+const clearDisplay = () => {
+  setDom.nameField.value = '';
+  setDom.displayLocation.innerHTML = '';
+  setDom.displayWeather.innerHTML = '';
+};
+
+const weekday = (value) => {
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  if (value > 7) {
+    value = -7;
+  }
+
+  return weekdays[value];
+};
+
+function displayWeather(current, forecast) {
+  clearDisplay();
+  if (current.cod != 200 || forecast.cod != 200) {
+    console.log('error');
+    return;
+  }
+
+  // current weather
+  const { name } = current;
+  const { country } = current.sys;
+  const { icon, main } = current.weather[0];
+  const { temp } = current.main;
   const ferhTemp = (temp * 9) / 5 + 32;
+  // tomorrow
+  const day1 = weekday(new Date().getDay() + 1);
+  const day1_icon = forecast.list[8].weather[0].icon;
+  const day1_description = forecast.list[8].weather[0].main;
+  const day1_temp = forecast.list[8].main.temp;
+  const day1_ferhTemp = (day1_temp * 9) / 5 + 32;
+  // day after
+  const day2 = weekday(new Date().getDay() + 2);
+  const day2_icon = forecast.list[16].weather[0].icon;
+  const day2_description = forecast.list[16].weather[0].main;
+  const day2_temp = forecast.list[16].main.temp;
+  const day2_ferhTemp = (day2_temp * 9) / 5 + 32;
 
   setDom.displayLocation.innerHTML = `Weather in ${name}, ${country}`;
 
   const weatherEl = document.createElement('div');
   weatherEl.innerHTML = `<div class="display-container">
-  <div class="data-section flex current">${Math.floor(temp)} C</div>
+  <div class="data-section flex current">${Math.round(temp)} C</div>
         <div class="data-section">
           <img
             src="http://openweathermap.org/img/wn/${icon}@2x.png"
@@ -55,19 +99,59 @@ function displayWeather(data) {
           <h4>${main}</h4>
         </div>
         <button class="units-btn flex">Metric</button>
-        </div>`;
+        </div>
+        <div class="display-days">
+        <div class="data-section">
+          <h4>${day1}</h4>
+          <div class="flex">
+            <div class="day1-temp">${Math.round(day1_temp)} C</div>
+            <div>
+              <img
+                src="http://openweathermap.org/img/wn/${day1_icon}@2x.png"
+                class="icon"
+              />
+              <h5>${day1_description}</h5>
+            </div>
+          </div>
+        </div>
+        <div class="data-section">
+          <h4>${day2}</h4>
+          <div class="flex">
+            <div class="day2-temp">${Math.round(day2_temp)} C</div>
+            <div>
+              <img
+                src="http://openweathermap.org/img/wn/${day2_icon}@2x.png"
+                class="icon"
+              />
+              <h5>${day2_description}</h5>
+            </div>
+          </div>
+        </div>
+      </div>`;
 
   setDom.displayWeather.appendChild(weatherEl);
 
   document.querySelector('.units-btn').addEventListener('click', (e) => {
     if (e.target.innerHTML == 'Metric') {
       e.target.innerHTML = 'Imperial';
-      document.querySelector('.current').innerHTML = `${Math.floor(
+      document.querySelector('.current').innerHTML = `${Math.round(
         ferhTemp
+      )} F`;
+      document.querySelector('.day1-temp').innerHTML = `${Math.round(
+        day1_ferhTemp
+      )} F`;
+      document.querySelector('.day2-temp').innerHTML = `${Math.round(
+        day2_ferhTemp
       )} F`;
     } else if (e.target.innerHTML == 'Imperial') {
       e.target.innerHTML = 'Metric';
-      document.querySelector('.current').innerHTML = `${Math.floor(temp)} F`;
+      document.querySelector('.current').innerHTML = `${Math.round(temp)} C`;
+      document.querySelector('.day1-temp').innerHTML = `${Math.round(
+        day2_temp
+      )} C`;
+      document.querySelector('.day2-temp').innerHTML = `${Math.round(
+        day2_temp
+      )} C`;
     }
   });
 }
